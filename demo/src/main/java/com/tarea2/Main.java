64d5cd901d27b83f5e47db466c38298c3535fc46
CapitanCigarro <nigarcia2023@inf.udec.cl>
2024-05-13 20:44:07 -0400

    test en main

--- demo/src/main/java/com/tarea2/Main.java
+++ demo/src/main/java/com/tarea2/Main.java
@@ -1,9 +1,20 @@
 package com.tarea2;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Empleados a = new Empleados("null", "null", "null", "null");
+        Empleados a = new Empleados("1", "null", "null", "null"), b = new Empleados("2", "null", "null", "null");
+        Empleados c = new Empleados("null","null", "null", "null");
+        ArrayList<Empleados> lE = new ArrayList<Empleados>();
+        lE.add(a);
+        lE.add(b);
+        Departamento dep = new Departamento("dep", lE);
+        
+        ReunionPresencial reunion = new ReunionPresencial(null, null, null, null, null, null)
 
-        System.out.println(a.toString());
+
+
+        System.out.println();
     }
 }
